a9df2484bc955670b893457a00dea8a7909d3888
tmatta <tyler.matta1@gmail.com>
2019-01-06 16:30:10 -0800

    updating equating script

--- R/02-univariate-equate-2.R
+++ R/02-univariate-equate-2.R
@@ -1,15 +1,14 @@
-
-
 #------------------------------------------------------------------------------#
 # univariate_equate.R equates benchmark scores to the anchor score scale.
-# Each time point is treated seperately.
+# Each time point is treated separately.
 # Equating methods are mean, linear and equipercentile
 #------------------------------------------------------------------------------#
 
-## Define relative paths
+# Define relative paths
 rDir <- "./R"
 rdsDir <- paste(rDir, "Rds", sep = "/")
 
+# load libraries
 library(equate)
 library(dplyr)
 
@@ -57,17 +56,18 @@ buildConcordance <- function(tt, eqType)
 #------------------------------------------------------------------------------#
 # Equate
 #------------------------------------------------------------------------------#
+# Mean equating
 eqMean <- lapply(seq_along(1:T), buildConcordance, eqType = "mean")
 
+# Linear equating
 eqLin <- lapply(seq_along(1:T), buildConcordance, eqType = "linear")
 
+# Equipercentile equating
 eqEqp <- lapply(seq_along(1:T), buildConcordance, eqType = "equipercentile")
 
 #------------------------------------------------------------------------------#
 # Save
 #------------------------------------------------------------------------------#
-
-## Save anchor and benchmark sum score
 saveRDS(eqMean, paste(rdsDir, "meanConcordance.Rds", sep = "/"))
 saveRDS(eqLin, paste(rdsDir, "linConcordance.Rds", sep = "/"))
 saveRDS(eqEqp, paste(rdsDir, "EqpConcordance.Rds", sep = "/"))
